fbeea78f31f00f0675c55a9f6fbb93445b78fb94
Jeanne Langevin-Robitaille <jeannerobitaille0@gmail.com>
2026-04-29 15:23:33 -0400

    Code de figure de comparaison

--- TestingFunctions/error_map.py
+++ TestingFunctions/error_map.py
@@ -1,26 +1,55 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import cv2
 
+# load data
 a = pd.read_csv(
     "TestingData/carte_erreur_thermique/FLIR0050_temperatures.csv",
     skiprows=5,
     header=None
-    ).to_numpy()
+).to_numpy()
+
+a_sim = pd.read_csv(
+    "TestingData/carte_erreur_thermique/simulation_temperatures_60s.csv",
+    skiprows=5,
+    header=None
+).to_numpy()
 
 b = pd.read_csv(
     "TestingData/carte_erreur_thermique/FLIR0051_temperatures.csv",
     skiprows=5,
     header=None
-    ).to_numpy()
+).to_numpy()
 
-print("shape of a is", a.shape)
-print("shape of b is", b.shape)
+b_sim = pd.read_csv(
+    "TestingData/carte_erreur_thermique/simulation_temperatures_final.csv",
+    skiprows=5,
+    header=None
+).to_numpy()
 
-plt.subplot(2,1,1)
-plt.imshow(a)
+# resizing
+a_sim_resized = cv2.resize(a_sim, (a.shape[1], a.shape[0]), interpolation=cv2.INTER_CUBIC)
+b_sim_resized = cv2.resize(b_sim, (b.shape[1], b.shape[0]), interpolation=cv2.INTER_CUBIC)
 
-plt.subplot(2,1,2)
-plt.imshow(b)
+# differences
+diff_a = a_sim_resized - a
+diff_b = b_sim_resized - b
+
+# ---- COMMON COLOR SCALE ----
+vmin = min(diff_a.min(), diff_b.min())
+vmax = max(diff_a.max(), diff_b.max())
+
+# plotting
+fig, axs = plt.subplots(2, 1)
+
+# Erreur avec une commande de 30°C après 60 s
+im1 = axs[0].imshow(diff_a, vmin=vmin, vmax=vmax)
+
+# Erreur avec une commande de 30°C après stabilitsation
+im2 = axs[1].imshow(diff_b, vmin=vmin, vmax=vmax)
+
+# single shared colorbar
+fig.colorbar(im2, ax=axs)
 
 plt.show()
