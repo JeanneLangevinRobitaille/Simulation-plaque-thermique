511ef9b882df9eb8da78d42e924a573cf83526be
Jeanne Langevin-Robitaille <jeannerobitaille0@gmail.com>
2026-04-28 20:19:10 -0400

    Extraction des données et affichage de surface thermique

--- TestsAndData/error_map.py
+++ TestsAndData/error_map.py
@@ -2,3 +2,26 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+a = pd.read_csv(
+    "TestsAndData/FLIR0050_temperatures.csv",
+    skiprows=5,
+    header=None
+    ).to_numpy()
+
+b = pd.read_csv(
+    "TestsAndData/FLIR0051_temperatures.csv",
+    skiprows=5,
+    header=None
+    ).to_numpy()
+
+print("shape of a is", a.shape)
+print("shape of b is", b.shape)
+
+plt.subplot(2,1,1)
+plt.imshow(a)
+
+plt.subplot(2,1,2)
+plt.imshow(b)
+
+
+plt.show()
